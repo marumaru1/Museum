00bd5f9ff0c18a606f420315b30fc0e70e36264a
andreypotkas <potkasa@gmail.com>
2021-10-05 17:37:30 +0300

    docs:museum

---
+++ museum/js/player.js
@@ -0,0 +1,13 @@
+const progress = document.querySelector('.progress');
+
+   
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+})
+ 
+const progressx = document.querySelector('.progressx');
+progressx.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`
+});
